79ff63c5e26067495912aa1af48e459f7dd8b940
Hangil Lee <rnbwave@users.noreply.github.com>
2022-03-31 20:11:50 -0700

    updating files

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
 function generatePassword(){
   //TODO: your code goes here
-  return "password"
-}
+  var length = 0;
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var num = "1234567890";
+  var spe = "!@#$%^&*()`~-_=+[]{}\|;:,./<>?";
+  var word = "";
+  var choo = "";
+  var password = "";
+  
+  length = prompt("Please choose a length of at least 8 characters and no more than 128 characters");
+  
+  if ( length < 8 || length > 128 ) {
+    alert("Please choose between 8 to 128.");
+    return "Please generate again.";
+  }
 
+  var incLow = confirm("Include lowercase characters?");
+  var incUpp = confirm("Include uppercase characters?");
+  var incNum = confirm("Include numeric characters?");
+  var incSpe = confirm("Include special characters?");
+
+  if (incLow) {
+    word = word + lower;
+    choo = choo + " lowercase";
+  }
+  if (incUpp) {
+    word = word + upper;
+    choo = choo + " uppercase";
+  }
+  if (incNum) {
+    word = word + num;
+    choo = choo + " numeric";
+  }
+  if (incSpe) {
+    word = word + spe;
+    choo = choo + " special";
+  }
+
+  alert("You choose" + choo + " characters.");
+
+  for (var i=0; i<length; i++) {
+    password = password + word.charAt(Math.floor(Math.random() * word.length));
+  }
+
+  return password;
+}
 
 // Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
 var generateBtn = document.querySelector("#generate");
